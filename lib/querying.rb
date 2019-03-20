624016228a79c602c7bd26424a58ec2a40df3159
Lizzie Chan <lchan217@gmail.com>
2019-03-20 22:31:37 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,7 +18,7 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
-  JOIN characters ON series.id = characters.series_id WHERE MAX COUNT(species) = 'human'"
+  JOIN characters ON series.id = characters.series_id WHERE COUNT(species) = 'human' DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
