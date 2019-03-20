b6d3da2b4b21e73be57ab9035efa3e404910908e
Lizzie Chan <lchan217@gmail.com>
2019-03-20 22:24:44 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,7 +11,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT subgenres.name, authors.name FROM subgenres JOIN series ON "
+  "SELECT subgenres.name, authors.name FROM subgenres JOIN series ON subgenres.id = "
 end
 
 def select_series_title_with_most_human_characters
