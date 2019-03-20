6c305bc45787e89477505524cda735b8fdc5daa9
Lizzie Chan <lchan217@gmail.com>
2019-03-20 22:35:43 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,5 +23,5 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(books.title) FROM characters
-  JOIN series ON characters."
+  JOIN series ON characters.id = "
 end
