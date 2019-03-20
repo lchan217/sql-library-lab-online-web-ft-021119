c1c2812118dd7c2e4b4021212597467697f04200
Lizzie Chan <lchan217@gmail.com>
2019-03-20 22:26:12 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,8 +12,8 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT subgenres.name, authors.name FROM subgenres 
-  JOIN series ON subgenres.id = series.subgenres_id
-  JOIN authors ON series.subgenres_id = authors.id"
+  JOIN series ON subgenre.id = series.subgenres_id
+  JOIN authors ON series.subgenre_id = authors.id"
 end
 
 def select_series_title_with_most_human_characters
